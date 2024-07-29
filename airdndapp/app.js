664f65275ba6adbc7c26f4f4c40cb7309ebb7d2c
likai0909 <23011764@myrp.edu.sg>
2024-07-29 10:28:28 +0800

    portchange

--- airdndapp/app.js
+++ airdndapp/app.js
@@ -189,7 +189,7 @@ app.get('/deleteHotel/:id', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
 });
